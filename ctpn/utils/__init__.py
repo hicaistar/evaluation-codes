5ad270106461e45053e66cabb4bb7b56ec7ce82d
hicaistar <wangshi@caicloud.io>
2019-04-11 14:26:13 +0800

    update

--- ctpn/utils/__init__.py
+++ ctpn/utils/__init__.py
@@ -1,5 +1,6 @@
-import metrics
-import text_connect_cfg
-import text_detector_api
-import text_proposal_connector
-import utils
+[python]
+from . import metrics
+from . import text_connect_cfg
+from . import text_detector_api
+from . import text_proposal_connector
+from . import utils
